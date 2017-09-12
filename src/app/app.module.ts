4441eeef43af7cfb848000d207991c37ad0d9797
khairahmanplus <khairahmanplus@gmail.com>
2017-09-12 15:33:11 +0800

    Add gitignote and some code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -30,6 +31,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -6,9 +8,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public date: string;
+  public events;
 
-  constructor() {
+  constructor(private http: Http) {}
 
+  getData() {
+    // Buat permintaan pada API
+    this.http.get('https://g5mhbciwo5.execute-api.ap-southeast-1.amazonaws.com/stage/events?eventdate='+this.date)
+    // Pemetaan
+    .map(res => res.json())
+    // Subscribe
+    .subscribe(
+        data => {
+          this.events = data;
+        },
+        // Jika ada error
+        err => console.log("error is "+err),
+        // Jika request complete
+        () => console.log('read events Complete '+ this.events)
+    );
   }
+
 }
- 
